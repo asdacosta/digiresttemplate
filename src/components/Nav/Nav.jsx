8907ac262e8a5ddc440045cd13fd15a9c6a52920
asdacosta <asdacosta18@gmail.com>
2024-10-01 09:57:30 +0000

    Add navigation

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,28 @@
+import styles from "./Nav.module.css";
+
+function Nav() {
+  return (
+    <section className={styles.nav}>
+      <section className={styles.logoBox}>
+        <h2>•Restaurant•</h2>
+      </section>
+      <section className={styles.right}>
+        <button>Home</button>
+        <button>About Us</button>
+        <button>Menu</button>
+        <button>Contact</button>
+        <button>Events</button>
+      </section>
+      {/* <button onClick={playMenu} className={styles.menu}>
+              <Player
+                ref={menuRef}
+                keepLastFrame={true}
+                src="https://raw.githubusercontent.com/asdacosta/digiAirX/main/src/assets/menu.json"
+                style={{ width: "50px", height: "50px" }}
+              ></Player>
+            </button> */}
+    </section>
+  );
+}
+
+export { Nav };
