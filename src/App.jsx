1792f549aa27cf58ef6d0799852b26804e661a3c
asdacosta <asdacosta18@gmail.com>
2024-10-01 09:56:17 +0000

    Add swipes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Nav } from "./components/Nav/Nav";
 import "./App.css";
 import "./reset.css";
+import { Swipes } from "./components/Nav/Swipes/Swipes";
 
 function App() {
   return (
     <>
       <Nav />
+      <Swipes />
     </>
   );
 }
