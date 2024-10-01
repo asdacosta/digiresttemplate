a293eb26d56b9adf6b01a5abbbe539d0a80e5d27
asdacosta <asdacosta18@gmail.com>
2024-10-01 12:39:36 +0000

    Import fixed parts

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Nav } from "./components/Nav/Nav";
 import "./App.css";
 import "./reset.css";
-import { Swipes } from "./components/Nav/Swipes/Swipes";
+import { Swipes } from "./components/Swipes/Swipes";
+import { FixedOrder } from "./components/FixedOrder/FixedOrder";
 
 function App() {
   return (
     <>
       <Nav />
       <Swipes />
+      <FixedOrder />
     </>
   );
 }
