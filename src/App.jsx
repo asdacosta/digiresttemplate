b9f48add8e251c9cc0cf6e21bd202fe4d184e5df
asdacosta <asdacosta18@gmail.com>
2024-10-01 18:04:15 +0000

    Remove Swipes component to add it in Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Nav } from "./components/Nav/Nav";
 import "./App.css";
 import "./reset.css";
-import { Swipes } from "./components/Swipes/Swipes";
 import { FixedOrder } from "./components/FixedOrder/FixedOrder";
+import { Home } from "./components/Home/Home";
 
 function App() {
   return (
     <>
       <Nav />
-      <Swipes />
+      <Home />
       <FixedOrder />
     </>
   );
