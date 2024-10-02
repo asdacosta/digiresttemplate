0eb1458eac3cdbb7e1bd13e89015301be38912f0
asdacosta <asdacosta18@gmail.com>
2024-10-02 21:09:15 +0000

    Remove unused import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home } from "./components/Home/Home.jsx";
 import { Menu } from "./components/Menu/Menu.jsx";
