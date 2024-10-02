f48f051277092857e44a30663a8f527184b1d81a
asdacosta <asdacosta18@gmail.com>
2024-10-02 15:59:31 +0000

    Add menu for mobile screens

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,13 +1,28 @@
 import styles from "./Nav.module.css";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { useRef, useState } from "react";
+import "@dotlottie/player-component";
 
 function Nav() {
+  const [menuOpened, setMenuOpened] = useState(false);
+  const menuRef = useRef(null);
+
+  const handleMenuClick = () => {
+    const menu = menuRef.current;
+    if (!menu) return;
+
+    setMenuOpened((prev) => !prev);
+    menu.setDirection(menuOpened ? -1 : 1);
+    menu.setSpeed(1.5);
+    menu.play();
+  };
+
   return (
     <section className={styles.nav}>
       <section className={styles.logoBox}>
         <h2>•Pizza•</h2>
       </section>
-      <section className={styles.right}>
+      <section className={`${styles.right} ${menuOpened ? styles.open : "open"}`}>
         <NavLink to="/" className={({ isActive }) => (isActive ? styles.active : "")}>
           <button>Home</button>
         </NavLink>
@@ -28,14 +43,14 @@ function Nav() {
           <button>Events</button>
         </NavLink>
       </section>
-      {/* <button onClick={playMenu} className={styles.menu}>
-              <Player
-                ref={menuRef}
-                keepLastFrame={true}
-                src="https://raw.githubusercontent.com/asdacosta/digiAirX/main/src/assets/menu.json"
-                style={{ width: "50px", height: "50px" }}
-              ></Player>
-            </button> */}
+      <button className={styles.menu}>
+        <dotlottie-player
+          ref={menuRef}
+          onClick={handleMenuClick}
+          src="https://raw.githubusercontent.com/asdacosta/digiresttemplate/main/src/assets/menu.lottie"
+          style={{ width: "4rem", height: "4rem" }}
+        ></dotlottie-player>
+      </button>
     </section>
   );
 }
