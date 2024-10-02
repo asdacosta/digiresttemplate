83056dc4baa0a253a4dc1f244e18c780536efb2c
asdacosta <asdacosta18@gmail.com>
2024-10-02 20:53:21 +0000

    Add scrolltotop to enable that upon every component mount

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { Nav } from "./components/Nav/Nav";
 import "./App.css";
 import "./reset.css";
 import { FixedOrder } from "./components/FixedOrder/FixedOrder";
-import { Home } from "./components/Home/Home";
 import { Outlet } from "react-router-dom";
+import { ScrollToTop } from "./components/ScrollToTop/ScrollToTop";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Nav />
       <Outlet />
       <FixedOrder />
